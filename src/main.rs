8c657a5216f6a610cf9383f54de37340865f522a
sillawned <30218969+sillawned@users.noreply.github.com>
2024-12-27 15:39:01 -0500

    stage 4

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::exit;
 
 fn main() {
         
-    // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
     let mut input = String::new();
     let stdin = io::stdin();
+    print!("$ ");
+    io::stdout().flush().unwrap();
 
     // Wait for user input
     while let Ok(_) = stdin.read_line(&mut input) {
-        print!("{}: command not found\n", input.trim());
+        match input.trim() {
+            "exit 0" => break,
+            _ => {
+                print!("{}: command not found\n", input.trim());
+            }
+            
+        }
         input.clear();
         print!("$ ");
         io::stdout().flush().unwrap();
     }
+
+    exit(0);
 }
