448cf6b112bdadee250c454240d89916a071c639
sillawned <30218969+sillawned@users.noreply.github.com>
2024-12-27 14:57:18 -0500

    stage 3

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,17 @@
 use std::io::{self, Write};
 
 fn main() {
+        
     // Uncomment this block to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+    let mut input = String::new();
+    let stdin = io::stdin();
 
     // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
-    print!("{}: command not found\n", input.trim());
+    while let Ok(_) = stdin.read_line(&mut input) {
+        print!("{}: command not found\n", input.trim());
+        print!("$ ");
+        io::stdout().flush().unwrap();
+    }
 }
