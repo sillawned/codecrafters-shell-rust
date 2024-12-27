c1e4e6c9de369577b967c4899568088fe852bf28
sillawned <30218969+sillawned@users.noreply.github.com>
2024-12-27 16:05:40 -0500

    stage 5

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,23 @@ fn main() {
         
     let mut input = String::new();
     let stdin = io::stdin();
+    let mut result: i32 = 0;
     print!("$ ");
     io::stdout().flush().unwrap();
 
     // Wait for user input
     while let Ok(_) = stdin.read_line(&mut input) {
-        match input.trim() {
-            "exit 0" => break,
+        let cmd_line: Vec<_> = input.trim().split(" ").collect();
+        match cmd_line[0] {
+            "exit" => {
+                result = cmd_line[1].parse().unwrap();
+                break;
+            },
+            "echo" => {
+                print!("{}\n", &cmd_line[1..].join(" "));
+            }
             _ => {
-                print!("{}: command not found\n", input.trim());
+                print!("{}: command not found\n", &cmd_line[0]);
             }
             
         }
@@ -23,5 +31,5 @@ fn main() {
         io::stdout().flush().unwrap();
     }
 
-    exit(0);
+    exit(result);
 }
